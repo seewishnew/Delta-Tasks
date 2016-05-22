be0cb1bf641a08c6ce6a8ccfccaec5acfcec5d8d
Vishnu <seawishnew@gmail.com>
2016-05-23 00:31:02 +0530

    Rectifies brainfarts

--- app/src/main/java/com/example/vishnu/firsttask/MainActivity.java
+++ app/src/main/java/com/example/vishnu/firsttask/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,7 +14,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static int count = 0;
+    public int count = 0;
 
 
     @Override
@@ -61,6 +62,6 @@ public class MainActivity extends AppCompatActivity {
     public void onClickListener(View view){
         count++;
 
-        Toast.makeText(MainActivity.this, count, Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, ""+count, Toast.LENGTH_SHORT).show();
     }
 }

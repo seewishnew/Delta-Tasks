f4236cd1395dd474692b8db27c1a98c30c62d1f2
Vishnu <seawishnew@gmail.com>
2016-05-23 10:48:09 +0530

    Major Changes Uses shared preferences in onPause to save during both app exits and orientation changes Random color changing functionality added Reset button added Color state does NOT persist during orientation changes

--- app/src/main/java/com/example/vishnu/firsttask/MainActivity.java
+++ app/src/main/java/com/example/vishnu/firsttask/MainActivity.java
@@ -1,25 +1,47 @@
 package com.example.vishnu.firsttask;
 
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import org.w3c.dom.Text;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String COUNT = "count";
     public int count = 0;
 
+    private SharedPreferences preferences;
+
+    private final String colors[] = {"red","blue","black","white","yellow","green"};
+
+    Random rand = new Random();
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +50,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        if(savedInstanceState!=null)
-        count = savedInstanceState.getInt(COUNT);
+        preferences = getPreferences(MODE_PRIVATE);
 
-        else
-        count = 0;
+        count = preferences.getInt(COUNT, 0);
 
         TextView textView = (TextView) findViewById(R.id.count);
 
@@ -48,14 +68,30 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+    protected void onPause() {
+        super.onPause();
 
-        outState.putInt(COUNT, count);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putInt(COUNT, count);
+
+        editor.commit();
+
+        Log.d("Main Activity", "Saved preferences");
     }
+
+    //    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//
+//        outState.putInt(COUNT, count);
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -79,12 +115,42 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onClickListener(View view){
+    public void onClickListener(View view) {
+
         count++;
 
         TextView textView = (TextView) findViewById(R.id.count);
 
         textView.setText("" + count + " times");
 
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        TextView textView1 = (TextView) findViewById(R.id.textView);
+        Button button = (Button) findViewById(R.id.button);
+        Button button1 = (Button) findViewById(R.id.reset);
+
+        int index = rand.nextInt(6);
+
+
+        relativeLayout.setBackgroundColor(Color.parseColor(colors[index]));
+
+        textView.setTextColor(Color.parseColor(colors[5 - index]));
+        textView1.setTextColor(Color.parseColor(colors[5 - index]));
+
+        button.setTextColor(Color.parseColor(colors[index]));
+        button.setBackgroundColor(Color.parseColor(colors[5 - index]));
+
+        button1.setTextColor(Color.parseColor(colors[index]));
+        button1.setBackgroundColor(Color.parseColor(colors[5 - index]));
+
+
     }
+
+
+    public void reset(View view){
+        count = 0;
+        TextView textView = (TextView) findViewById(R.id.count);
+
+        textView.setText("" + count + " times");
+    }
+
 }
